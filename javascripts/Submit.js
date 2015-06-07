b4650a7c0175b0366ca42cfc6d27dccbf50cc0ab
Tom Kelly <tkelly@scottlogic.co.uk>
2015-06-07 17:25:38 +0100

    Add initial client-side socket connection
    Tries to communicate with the page for the server-side repository and
    send a message.

--- javascripts/Submit.js
+++ javascripts/Submit.js
@@ -3,8 +3,12 @@ angular.module('roles', [])
 	    $scope.formClass = 'visible';
 		$scope.waitingClass = 'initially-hidden';
 	
+		var socket = new WebSocket('ws://tpkelly.github.io/roles-host/', ['soap']);
+	
 		$scope.submitName = function() {
 		  $scope.formClass = 'animate-hidden';
 		  $scope.waitingClass = 'visible';
+		  
+		  socket.send($scope.badgename);
 		}
 	}]);
