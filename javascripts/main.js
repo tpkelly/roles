3ed521bb165f8f8a0f4d7b624b165f23d7f3e4ad
Tom Kelly <tkelly@scottlogic.co.uk>
2015-06-07 15:54:55 +0100

    Add second screen to the form
    After submitting, the form fades out and a new screen fades in.
    For now, this just echoes back the badgename and claims we are waiting
    to start.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,10 @@
 angular.module('roles', [])
 	.controller('SubmitController', ['$scope', function($scope) {
+	    $scope.formClass = 'visible';
+		$scope.waitingClass = 'initially-hidden';
+	
 		$scope.submitName = function() {
+		  $scope.formClass = 'animate-hidden';
+		  $scope.waitingClass = 'visible';
 		}
-		
 	}]);
