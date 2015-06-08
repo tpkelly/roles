22b669430890fc59b11214e6b9ea37768964015f
Tom Kelly <tkelly@scottlogic.co.uk>
2015-06-08 11:56:53 +0100

    Redirect hosts to the heroku Websockets
    We are now hosting the server side of the websockets app on heroku and
    need to point our app at the correct server.

--- javascripts/Submit.js
+++ javascripts/Submit.js
@@ -3,7 +3,12 @@ angular.module('roles', [])
 	    $scope.formClass = 'visible';
 		$scope.waitingClass = 'initially-hidden';
 	
-		var socket = new WebSocket('ws://tpkelly.github.io/roles-host/', ['soap']);
+		var socket = new WebSocket('ws://roles-host.herokuapp.com');
+		socket.onopen = function() {};
+		
+		socket.onmessage = function(message) {
+		  console.log(message);
+		};
 	
 		$scope.submitName = function() {
 		  if ($scope.badgename == null || $scope.badgename.length === 0) {
