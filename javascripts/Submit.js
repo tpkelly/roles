b367f813a2b424077ff31ce980c72aebb7a6c018
tkelly <tkelly@scottlogic.co.uk>
2015-06-17 11:17:39 +0100

    Dismiss notification when moderator acknowledges
    Instead of re-enabling the button a few seconds later, we now wait for
    the moderator to dismiss the notification

--- javascripts/Submit.js
+++ javascripts/Submit.js
@@ -1,56 +1,58 @@
 angular.module('roles', [])
-	.controller('SubmitController', ['$scope', function($scope) {
-	    $scope.formClass = 'visible';
-		$scope.waitingClass = 'initially-hidden';
-		$scope.role = null;
-		$scope.countdown = null;
+    .controller('SubmitController', ['$scope', function($scope) {
+        $scope.formClass = 'visible';
+        $scope.waitingClass = 'initially-hidden';
+        $scope.role = null;
+        $scope.countdown = null;
     $scope.moderatorCalled = false;
     
-	    var interval;
-	    var countdown = function() {
-			if ($scope.countdown <= -5) {
-			  clearInterval(interval);
-			  $scope.role = null;
-			  return;
-			}
-			$scope.countdown--;
+        var interval;
+        var countdown = function() {
+            if ($scope.countdown <= -5) {
+              clearInterval(interval);
+              $scope.role = null;
+              return;
+            }
+            $scope.countdown--;
             $scope.$apply();
-		}
-	
-		var socket = new WebSocket('ws://roles-host.herokuapp.com');
-		socket.onopen = function() {};
-		
-		socket.onmessage = function(message) {
-		  var data = JSON.parse(message.data);
-		  if (data.role) {
-		       $scope.countdown = 5;
-			   $scope.role = data.role;
-			   $scope.$apply();
-			   interval = setInterval(countdown, 1000);
-		  }
-		};
-	
-		$scope.submitName = function() {
-		  if ($scope.badgename == null || $scope.badgename.length === 0) {
-		    return;
-	      }
-		
-		  $scope.formClass = 'animate-hidden';
-		  $scope.waitingClass = 'visible';
-		  
-		  
-		  var message = { name: $scope.badgename, type: 'join' };
-		  socket.send(JSON.stringify(message));
-		}
+        }
+    
+        var socket = new WebSocket('ws://roles-host.herokuapp.com');
+        socket.onopen = function() {};
+        
+        socket.onmessage = function(message) {
+          if (message.data === 'dismiss') {
+            $scope.moderatorCalled = false;
+            $scope.$apply();
+            return;
+          }
+    
+          var data = JSON.parse(message.data);
+          if (data.role) {
+               $scope.countdown = 5;
+               $scope.role = data.role;
+               $scope.$apply();
+               interval = setInterval(countdown, 1000);
+          }
+        };
+    
+        $scope.submitName = function() {
+          if ($scope.badgename == null || $scope.badgename.length === 0) {
+            return;
+          }
+        
+          $scope.formClass = 'animate-hidden';
+          $scope.waitingClass = 'visible';
+          
+          
+          var message = { name: $scope.badgename, type: 'join' };
+          socket.send(JSON.stringify(message));
+        }
     
     $scope.callMod = function() {
       var message = { type: 'callModerator' };
       socket.send(JSON.stringify(message));
       
       $scope.moderatorCalled = true;
-      setTimeout(function() {
-        $scope.moderatorCalled = false;
-        $scope.$apply();
-      }, 5000);
     }
-	}]);
+    }]);
